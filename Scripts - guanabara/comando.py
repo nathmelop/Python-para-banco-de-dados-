0f804056feb314ef502d18c1ff963a3e402ae928
nathmelop <nathaliapmelo@hotmail.com>
2021-06-28 13:25:29 -0300

    update

--- Scripts - guanabara/comando.py
+++ Scripts - guanabara/comando.py
@@ -3,7 +3,7 @@ idade = input('Qual é a sua idade?')
 peso = input ('Qual é o seu peso?')
 print(nome, idade, peso)
 
-Desafio 01 
+
 
 nome = input ('Qual é o seu nome?')
 print('Olá', nome , '! Prazer em te conhecer!')
---
+++ Scripts - guanabara/primeiroPassos.py
@@ -0,0 +1,5 @@
+numero = input('primeiro numero')
+numero2 = input('segundo numero')
+soma = int (numero) + int (numero2)
+print('A soma é ' + str (soma))
+
