0ea22d817ebb48ef941f7ef773efdcb9b2a2ed8e
nathmelop <nathaliapmelo@hotmail.com>
2021-06-28 13:25:00 -0300

    update

---
+++ Scripts - guanabara/comando.py
@@ -0,0 +1,25 @@
+nome = input('Qual é o seu nome?')
+idade = input('Qual é a sua idade?')
+peso = input ('Qual é o seu peso?')
+print(nome, idade, peso)
+
+Desafio 01 
+
+nome = input ('Qual é o seu nome?')
+print('Olá', nome , '! Prazer em te conhecer!')
+
+desafio 02 
+
+dia = input('Que dia você nasceu?')
+mes = input ('Que mês você nasceu?')
+ano = input ('Qual ano você nasceu?')
+print(dia,'/',mes,'/',ano) 
+print('Você nasceu no dia', dia,'de', mes,'de',ano,'.Correto?')
+
+Desafio 03 
+
+numero = input('primeiro numero')
+numero2 = input('segundo numero')
+soma = int (numero) + int (numero2)
+print('A soma é ' + str (soma))
+
