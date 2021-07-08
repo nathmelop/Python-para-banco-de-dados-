8bd17b783a491896758fbff2794d1c5297e4b0bd
nathmelop <nathaliapmelo@hotmail.com>
2021-07-08 12:05:21 -0300

    ex 01 - 011

--- Scripts - guanabara/ex001-005.py
+++ Scripts - guanabara/ex001-005.py
@@ -56,3 +56,39 @@ dm = n * 10
 cm = n * 100
 mm = n * 1000
 print('A medida de {} corresponde  a \n {}km \n {}hm \n {}dam \n {}dm \n {}cm \n {}mm'.format(n,k,h,d,dm,cm,mm))
+
+#9 
+
+n = int(input('Digite um numero para visualizar sua tabuada:'))
+n1 = n * 1
+n2 = n * 2
+n3 = n * 3
+n4 = n * 4
+n5 = n * 5
+n6 = n * 6
+n7 = n * 7
+n8 = n * 8
+n9 = n * 9
+n10 = n * 10
+print('{} X 1 = {}'.format(n,n1))
+print('{} X 2 = {}'.format(n,n2))
+print('{} X 3 = {}'.format(n,n3))
+print('{} x 4 = {}'.format(n,n4))
+print('{} X 5 = {}'.format(n,n5))
+print('{} X 6 = {}'.format(n,n6))
+print('{} X 7 = {}'.format(n,n7))
+print('{} X 8 = {}'.format(n,n8))
+print('{} X 9 = {}'.format(n,n9))
+print('{} X 10 = {}'.format(n,n10))
+
+#10
+
+n = float(input('Transformador de dolar para real:'))
+print('US{} Equivale a R${:.2f} .'.format(n,3.27 * n))
+
+#11
+
+largura =float(input('Qual a largura da sua parede?'))
+altura =float(input('Qual a altura da sua parede?'))
+area = largura * altura
+print ('Sua parede tem a dimensao de {}X{} e sua área é de {}m.'.format(largura,altura,area))
